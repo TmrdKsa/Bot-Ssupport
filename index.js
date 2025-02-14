825fa114f03bbe7ce66c2cef2a98b5e34773d97b
TmrdKsa <49895132+TmrdKsa@users.noreply.github.com>
2025-02-14 09:06:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
-const play = require('play-dl'); 
-require('dotenv').config();
-const fs = require('fs');
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayerStatus,
+  VoiceConnectionStatus,
+} = require("@discordjs/voice");
+const play = require("play-dl");
+require("dotenv").config();
+const fs = require("fs");
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
-const express = require("express")
+const express = require("express");
 const app = express();
 var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
 app.listen(() => console.log("I'm Ready To Work..! 24H"));
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(`
   <body>
   <center><h1>Bot 24H ON!</h1></center
-  </body>`)
+  </body>`);
 });
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`✅ | Logged in as ${client.user.tag}`);
 });
 
+const voiceChannelId = "1335993267959107696";
+const staffChannelID = "1335993267812044916";
+const staffRoleID = "1335993265782001835";
+const audioFilePath = "./support.mp3";
 
-const voiceChannelId = '';
-const staffChannelID = '';
-const staffRoleID = '';
-const audioFilePath = './support.mp3'; 
-
-
-client.on('voiceStateUpdate', (oldState, newState) => {
-  if(newState.member.user.bot) return;
-  if (newState.channelId === voiceChannelId && oldState.channelId !== voiceChannelId) {
+client.on("voiceStateUpdate", (oldState, newState) => {
+  if (newState.member.user.bot) return;
+  if (
+    newState.channelId === voiceChannelId &&
+    oldState.channelId !== voiceChannelId
+  ) {
     const channel = newState.channel;
     const connection = joinVoiceChannel({
       channelId: channel.id,
@@ -43,11 +55,11 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     player.play(resource);
     connection.subscribe(player);
     const staffChannel = newState.guild.channels.cache.get(staffChannelID);
-    
-    if(staffChannel) {
-        staffChannel.send(`!شخضأ ما يحتاج مساعدة \n<@&${staffRoleID}>`);
+
+    if (staffChannel) {
+      staffChannel.send(`!شخضأ ما يحتاج مساعدة \n<@&${staffRoleID}>`);
     } else {
-        console.log('ايدي روم الادارة غير مسجل او خطأ');
+      console.log("ايدي روم الادارة غير مسجل او خطأ");
     }
   }
 });
